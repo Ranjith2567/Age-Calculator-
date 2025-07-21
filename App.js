d13176eb6479317db4810fe1094f4e15dce2e8b0
Ranjith4356 <garanjith4257@gmail.com>
2025-07-21 21:31:09 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import AgeCalculator from './Components/AgeCalculator';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return <AgeCalculator />;
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import AgeCalculator from './Components/AgeCalculator';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<AgeCalculator />);
